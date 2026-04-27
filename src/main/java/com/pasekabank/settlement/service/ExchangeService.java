2eb6aa12c17678e15d1fed00a68396f7d9cb6fd3
paseka <pasekamabitsela22@gmail.com>
2026-04-27 17:31:30 +0200

    done with exchange service error handling and added calculateExchangeRate method that holds the final value with  setscale method to round half up , rounded to 2

--- src/main/java/com/pasekabank/settlement/service/ExchangeService.java
+++ src/main/java/com/pasekabank/settlement/service/ExchangeService.java
@@ -1,42 +1,44 @@
 package com.pasekabank.settlement.service;
 
 import java.math.BigDecimal;
-
-
-
+import java.math.RoundingMode;
 
 public class ExchangeService {
     private BigDecimal exchangeRate;
     private BigDecimal initialAmount;
 
-
+    //
     public ExchangeService(BigDecimal exchangeRate, BigDecimal initialAmount) {
-
         this.exchangeRate = exchangeRate;
         this.initialAmount = initialAmount;
-
     }
 
-
     public BigDecimal getExchangeRate() {
-
-        if  (exchangeRate == null) {
-            throw new IllegalArgumentException("Exchange rate is cannot be null  ");
+        if (exchangeRate == null) {
+            throw new IllegalArgumentException("Exchange rate cannot be null.");
         }
-
+        if (exchangeRate.compareTo(BigDecimal.ZERO) <= 0) {
+            throw new IllegalArgumentException("Exchange rate must be greater than zero.");
+        }
         return exchangeRate;
     }
 
     public BigDecimal getInitialAmount() {
+        if (initialAmount == null) {
+            throw new IllegalArgumentException("Initial amount cannot be null.");
+        }
+        // negative money
+        if (initialAmount.compareTo(BigDecimal.ZERO) < 0) {
+            throw new IllegalArgumentException("Amount to exchange cannot be negative.");
+        }
         return initialAmount;
-
     }
 
-    public  BigDecimal CalculateExchangeRate( ) {
+    public BigDecimal calculateExchangeRate() {
 
-        BigDecimal multiply = getExchangeRate().multiply(getInitialAmount());
-        return multiply;
+        BigDecimal result = getExchangeRate().multiply(getInitialAmount());
 
 
+        return result.setScale(2, RoundingMode.HALF_UP);
     }
 }
