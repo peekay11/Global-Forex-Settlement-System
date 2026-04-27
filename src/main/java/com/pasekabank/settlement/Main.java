aeaedf32e3dc751b27592b1c9cc8919395a4787d
paseka <pasekamabitsela22@gmail.com>
2026-04-27 13:23:57 +0200

    project start

---
+++ src/main/java/com/pasekabank/settlement/Main.java
@@ -0,0 +1,10 @@
+package com.pasekabank.settlement;
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+    public void main (String[] args){
+
+        System.out.println("hi there");
+    }
+}
